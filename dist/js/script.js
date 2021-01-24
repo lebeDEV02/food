5512407defa8649646ffcb262ec7d5e78266483e
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-01-24 14:50:33 +0500

    added rest operator to class and foreseed oportunity to prevent correct work DynamicCard class, so, added default values

--- dist/js/script.js
+++ dist/js/script.js
@@ -208,19 +208,27 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('scroll', checker);
 
   class DymanicCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.scr = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
+      this.classes = classes;
       this.price = price;
       this.parent = document.querySelector(parentSelector);
     }
 
     createElement() {
       const div = document.createElement('div');
+
+      if (this.classes.length === 0) {
+        this.div = 'menu__item';
+        div.classList.add(this.div);
+      } else {
+        this.classes.forEach(className => div.classList.add(className));
+      }
+
       div.innerHTML = `
-			<div class="menu__item">
 					<img src=${this.scr} alt=${this.alt}>
 					<h3 class="menu__item-subtitle">${this.title}</h3>
 					<div class="menu__item-descr">${this.descr}</div>
@@ -229,16 +237,15 @@ document.addEventListener('DOMContentLoaded', () => {
 						<div class="menu__item-cost">Цена:</div>
 						<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
 					</div>
-				</div>
 	`;
       this.parent.append(div);
     }
 
   }
 
-  new DymanicCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 229, '.menu .container').createElement();
-  new DymanicCard("img/tabs/elite.jpg", "elite", 'Меню "Премиум"', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 550, '.menu .container').createElement();
-  new DymanicCard("img/tabs/post.jpg", "post", 'Меню "Постное"', 'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за cчет тофу и импортных вегетарианских стейков', 340, '.menu .container').createElement();
+  new DymanicCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 229, '.menu .container', 'menu__item').createElement();
+  new DymanicCard("img/tabs/elite.jpg", "elite", 'Меню "Премиум"', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 550, '.menu .container', 'menu__item').createElement();
+  new DymanicCard("img/tabs/post.jpg", "post", 'Меню "Постное"', 'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за cчет тофу и импортных вегетарианских стейков', 340, '.menu .container', 'menu__item').createElement();
 });
 
 /***/ })
